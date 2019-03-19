c950f1e802de77f40f618436f54e5e3f4af87c6b
miriammp <miriam.mendoza@interseguro.com.pe>
2019-03-19 12:09:03 -0500

    TODO: SCSS - PUG - BOOTSTRAP - COMPONENT.

---
+++ pages/index.vue
@@ -0,0 +1,54 @@
+<template lang="pug">
+  section.container
+    div
+      airbnb-date-picker
+      h1.title test-airbnb-date
+      h2.subtitle
+        | My classy Nuxt.js project
+      .links
+        a(href="https://nuxtjs.org/" target="_blank" class="button--green") Documentation
+        a(href="https://github.com/nuxt/nuxt.js" target="_blank" class="button--grey") GitHub
+</template>
+
+<script>
+import AirbnbDatePicker from '~/components/AirbnbDatePicker.vue'
+
+export default {
+  components: {
+    AirbnbDatePicker
+  }
+}
+</script>
+
+<style>
+.container {
+  margin: 0 auto;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+
+.title {
+  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
+    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
+  display: block;
+  font-weight: 300;
+  font-size: 100px;
+  color: #35495e;
+  letter-spacing: 1px;
+}
+
+.subtitle {
+  font-weight: 300;
+  font-size: 42px;
+  color: #526488;
+  word-spacing: 5px;
+  padding-bottom: 15px;
+}
+
+.links {
+  padding-top: 15px;
+}
+</style>
